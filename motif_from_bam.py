b95d8dc61b40b13f52d1ba553dfba7721ba49540
Daniel Rawlinson <daniel.rawlinson@unimelb.edu.au>
2020-03-14 18:56:27 +1100

    initial commit

--- motif_from_bam.py
+++ motif_from_bam.py
@@ -20,11 +20,11 @@ for a in sam.fetch('MT007544.1', start = 65, stop = 75):
 		motif_segment = ''
 		if p[1] > 75:
 			break
-		elif p[1] >= 64 && p[1] <= 74:
-		if p[0] != None:
-			motif_segment+=p[0]
-		else:
-			motif_segment+='-'
+		elif p[1] >= 64 and p[1] <= 74:
+            if p[0] != None:
+                motif_segment+=p[0]
+            else:
+                motif_segment+='-'
 	motif_seqs.append(motif_segment)
 		
 	
