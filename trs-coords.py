0eb3f01eb28309b2ef1c973c828c5d372eb65ab2
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-04-21 00:23:34 +1000

    Initial commit

---
+++ trs-coords.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--TRS_seq', type=str,description='Conserved Transcription Regulating Sequence to find', default='ACGAAC', required=False)
+parser.add_argument('--virus_genome', type=str, description='Location of viral genome fasta file', required=True)
+args = parser.parse_args()
+
+lead100 = SeqIO.read(args.virus_genome, 'fasta')[0:99]
+
+loc = lead100.find(args.TRS_seq)
+if loc == -1:
+    sys.exit('Error: Failed to find given TRS sequence in first 100bp of genome')
+else:
+    start, stop = [loc+1, loc+len(args.TRS_seq)+1] #correct for 0-based index
+
+
+        
