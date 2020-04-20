2ad1c69dd8a0573ef3b66d622e8b847bb1e2531e
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-04-21 01:00:08 +1000

    add metavar

--- trs-coords.py
+++ trs-coords.py
@@ -5,17 +5,17 @@ import sys
 from Bio import SeqIO
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--TRS_seq', type=str,help='Conserved Transcription Regulating Sequence to find', default='ACGAAC', required=False)
-parser.add_argument('--virus_genome', type=str, help='Location of viral genome fasta file', required=True)
+parser.add_argument('--TRS_seq', metavar='STRING'  type=str,help='Conserved Transcription Regulating Sequence to find. Default= ACGAAC ', default='ACGAAC', required=False)
+parser.add_argument('--virus_genome', metavar='FASTA',type=str, help='Location of viral genome fasta file', required=True)
 args = parser.parse_args()
 
-lead100 = SeqIO.read(args.virus_genome, 'fasta')[0:99]
+genome = SeqIO.read(args.virus_genome, 'fasta')[0:99]
 
-loc = lead100.seq.find(args.TRS_seq)
+loc = genome.seq.find(args.TRS_seq, end=99)
 if loc == -1:
     sys.exit('Error: Failed to find given TRS sequence in first 100bp of genome')
 else:
-    start, stop = [loc+1, loc+len(args.TRS_seq)+1] #correct for 0-based index
-
+    start, stop = [loc+1, loc+len(args.TRS_seq)] #correct for 0-based index
+  
 
         
