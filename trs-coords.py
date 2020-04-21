39d4bd17721c78539eda42907a97fd5aa6255ee4
dn-ra <daniel.rawlinson@unimelb.edu.au>
2020-04-21 15:34:57 +1000

    output coords to file

--- trs-coords.py
+++ trs-coords.py
@@ -3,19 +3,33 @@
 import argparse
 import sys
 from Bio import SeqIO
+import re
+import csv
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--TRS_seq', metavar='STRING',  type=str,help='Conserved Transcription Regulating Sequence to find. Default= ACGAAC ', default='ACGAAC', required=False)
+parser.add_argument('--TRS_short', metavar='STRING',  type=str,help='Conserved Transcription Regulating Sequence to find. Default= ACGAAC ', default='ACGAAC', required=False)
 parser.add_argument('--virus_genome', metavar='FASTA',type=str, help='Location of viral genome fasta file', required=True)
 args = parser.parse_args()
 
-genome = SeqIO.read(args.virus_genome, 'fasta')[0:99]
+genome = SeqIO.read(args.virus_genome, 'fasta')
 
-loc = genome.seq.find(args.TRS_seq, end=99)
+
+#locs = re.match(args.TRS_short, str(genome.seq))
+loc = genome.seq.find(args.TRS_short, end=99)
 if loc == -1:
     sys.exit('Error: Failed to find given TRS sequence in first 100bp of genome')
-else:
-    start, stop = [loc+1, loc+len(args.TRS_seq)] #correct for 0-based index
-  
+
+#location of TRS-L
+start, stop = [loc+1, loc+len(args.TRS_seq)] #correct for 0-based index
+
+#exact matching for short TRS-B
+
+with open('TRS_short.coords.csv', 'w') as f:
+    f_write = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE)
+    f_write.writerow(['Start', 'End', 'Sequence'])
+    for t in re.finditer(args.TRS_short, str(genome.seq)):
+        f_write.writerow([t.span(0)[0]+1,t.span(0)[1]+1, args.TRS_short])
 
         
+    
+    
