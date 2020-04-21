917ab371eeafe70dbd23b173b5218a3127ca709d
dn-ra <daniel@login.novalocal>
2020-04-21 16:31:27 +1000

    full function

--- trs-coords.py
+++ trs-coords.py
@@ -14,22 +14,18 @@ args = parser.parse_args()
 genome = SeqIO.read(args.virus_genome, 'fasta')
 
 
-#locs = re.match(args.TRS_short, str(genome.seq))
 loc = genome.seq.find(args.TRS_short, end=99)
 if loc == -1:
     sys.exit('Error: Failed to find given TRS sequence in first 100bp of genome')
 
 #location of TRS-L
-start, stop = [loc+1, loc+len(args.TRS_seq)] #correct for 0-based index
+start, stop = [loc+1, loc+len(args.TRS_short)] #correct for 0-based index
 
-#exact matching for short TRS-B
+#exact matching for short TRS-B. write straight to file
 
 with open('TRS_short.coords.csv', 'w') as f:
     f_write = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE)
     f_write.writerow(['Start', 'End', 'Sequence'])
     for t in re.finditer(args.TRS_short, str(genome.seq)):
-        f_write.writerow([t.span(0)[0]+1,t.span(0)[1]+1, args.TRS_short])
+        f_write.writerow([t.span(0)[0]+1,t.span(0)[1], args.TRS_short])
 
-        
-    
-    
